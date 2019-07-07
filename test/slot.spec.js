92209a7755cd7841641c03020627b8e994c6eb98
rixo <eric@rixo.fr>
2019-07-07 03:15:04 +0200

    add slot tests

--- test/slot.spec.js
+++ test/slot.spec.js
@@ -6,43 +6,7 @@ const {
   innerText,
 } = require('../test-utils/testHmr')
 
-describe('basic HMR', () => {
-  testHmr('updates text content (twice)', function*() {
-    expect(yield innerText('h1')).to.equal('Hello world!')
-
-    yield change({
-      'App.svelte': '<h1>HMRd</h1>',
-    })
-    expect(yield innerText('h1')).to.equal('HMRd')
-
-    yield change({
-      'App.svelte': '<h1>reHMRd</h1>',
-    })
-    expect(yield innerText('h1')).to.equal('reHMRd')
-  })
-
-  testHmr('updates child text when child changes', function*() {
-    yield templates({
-      'App.svelte': slot => `
-        <script>
-          import Child from './Child'
-        </script>
-        ${slot}
-      `,
-    })
-
-    yield init({
-      'App.svelte': '<Child />',
-      'Child.svelte': '<h2>I am Child</h2>',
-    })
-    expect(yield innerText('h2')).to.equal('I am Child')
-
-    yield change({
-      'Child.svelte': '<h2>I am Kid</h2>',
-    })
-    expect(yield innerText('h2')).to.equal('I am Kid')
-  })
-
+describe('HMR (slots)', () => {
   testHmr('updates default slot when parent changes', function*() {
     yield templates({
       'App.svelte': slot => `
