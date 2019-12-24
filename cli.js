201054bb5b53dc9f8196288909a43035d5354ad6
rixo <rixo@rixo.fr>
2019-12-24 02:35:36 +0100

    simpler run for easier third party consumption

--- cli.js
+++ cli.js
@@ -2,28 +2,6 @@
 
 const impørt = require('esm')(module)
 
-const path = impørt('path')
+const { run } = impørt('./lib/cli')
 
-const { findRc, run } = impørt('./lib/cli')
-
-const cwd = process.cwd()
-
-findRc(cwd)
-  .then(rcFile => {
-    if (!rcFile) {
-      // eslint-disable-next-line no-console
-      console.error('Failed to find .thcrc.js file from %s', cwd)
-      process.exit(1)
-    }
-    const options = impørt(rcFile)
-    const args = process.argv.slice(1)
-    return run({
-      root: path.dirname(rcFile),
-      ...options,
-      args,
-    })
-  })
-  .catch(err => {
-    // eslint-disable-next-line no-console
-    console.error(err)
-  })
+run()
