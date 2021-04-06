4d258cc0213b8626ecd21ba880afc5c27f45ccea
rixo <rixo@rixo.fr>
2021-04-07 00:37:52 +0200

    revert to cjs to avoid forcing esm on consumer

--- index.js
+++ index.js
@@ -2,4 +2,8 @@
  * Index
  */
 
-export { browse, fixture, dev, test, describe } from './lib/harness.js'
+// eslint-disable-next-line no-global-assign
+require = require('esm')(module)
+
+// export { browse, fixture, dev, test, describe } from './lib/harness.js'
+module.exports = require('./lib/harness.js')
